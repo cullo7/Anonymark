5259020c2d536fe34b21f996e8f0d76f8f695eab
cullo7 <culloaiden3@gmail.com>
2016-11-12 22:56:20 -0500

    works on all pngs

--- tester.sh
+++ tester.sh
@@ -1,6 +1,6 @@
 COUNTER=1
 while [ $COUNTER -lt 10 ]; do
 	echo "$COUNTER"
-	python3 main.py test_suite/$COUNTER.png test_suite/Altered.png
+	python3 main.py test_suite/$COUNTER.png test_suite/Altered.png test_suite/1.png
 	let COUNTER+=1
 done
