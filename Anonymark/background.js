3823f77ad9fff1f505efb2d214444cb8573d7b2d
Ethan Schoen <e.schoen@yahoo.com>
2016-11-13 02:07:17 -0500

    lots of small changes and cleaning up

--- Anonymark/background.js
+++ Anonymark/background.js
@@ -1,3 +1,62 @@
+function loadScript(url, callback) {
+    // Adding the script tag to the head as suggested before
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+    // Then bind the event to the callback function.
+    // There are several events for cross browser compatibility.
+    script.onreadystatechange = callback;
+    script.onload = callback;
+    // Fire the loading
+    head.appendChild(script);
+}
+function decrypt() { // call encryption JS here
+    var encrypted_url = window.location.href;
+    encrypted_url = url.substring(url.indexOf('#') + 1);
+    img_decrypt(url, fileChooser.files[0]); // pass to other JS file
+}
+var script_load = function() {
+    decrypt("Hello!");
+};
+/* Creates an `input[type="file]` */
+var fileChooser = document.createElement('input');
+// var file;
+fileChooser.type = 'file';
+fileChooser.addEventListener('change', function () {
+    // var file = fileChooser.files[0];
+    loadScript('resources/encrypt.js', script_load); // laod auxilary JS file
+    // var reader = new FileReader();
+    // reader.onloadend = function () {
+    //     image_str = reader.result;
+    //     loadScript('resources/encrypt.js', script_load);
+    // }
+    // reader.readAsDataURL(file);
+
+    // var img = document.getElementById("hash");
+    // img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzK4Lu0EoLHfQFlkDWCLhppwbrx6dCXGsh0_6Lv4mdR-vM1IdavA";
+    // var div = document.getElementById("decoded");
+    // div.innerHTML = "test";
+});
+/* Wrap it in a form for resetting */
+// var form = document.createElement('form');       Maybe these two lines are not needed
+// form.appendChild(fileChooser);
+/* Listen for messages from popup */
+chrome.runtime.onMessage.addListener(function (msg) {
+    if (msg.action === 'browseAndUpload') {
+        fileChooser.click();
+    }
+});
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('button').addEventListener('click', function () {
+        chrome.runtime.sendMessage({ action: 'browseAndUpload' });
+    });
+});
+
+function renderImage(url) {
+    img = document.getElementById('hash');
+    img.src = url;
+}
 function getClickHandler() {
     return function(info) {
         var url = 'display.html#' + info.srcUrl;
@@ -5,43 +64,9 @@ function getClickHandler() {
         renderImage(info);
     };
 };
-function decrypt(url) {
-    $.ajax({
-        type: "POST",
-        url: "../main.py",
-        data: {param: url}
-    }).done(function ( o ) {
-    });
-}
 chrome.contextMenus.create({
     "title" : "Decrypt Image",
     "type" : "normal",
     "contexts" : ["image"],
     "onclick" : getClickHandler()
-});
-
-
-var uploadUrl = window.location.href
-/* Creates an `input[type="file]` */
-var fileChooser = document.createElement('input');
-fileChooser.type = 'file';
-fileChooser.addEventListener('change', function () {
-    var file = fileChooser.files[0];
-    var reader = new FileReader();
-    reader.onloadend = function () {
-        console.log(reader.result);
-        renderImage(reader.result);
-    }
-    reader.readAsDataURL(file);
-});
-
-/* Wrap it in a form for resetting */
-var form = document.createElement('form');
-form.appendChild(fileChooser);
-
-/* Listen for messages from popup */
-chrome.runtime.onMessage.addListener(function (msg) {
-    if (msg.action === 'browseAndUpload') {
-        fileChooser.click();
-    }
 });
