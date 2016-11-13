c733ba94a1f99bf465a12ecd992a84f37c075b5f
Ethan Schoen <e.schoen@yahoo.com>
2016-11-13 03:35:59 -0500

    looks wayy prettier now

--- Anonymark/background.js
+++ Anonymark/background.js
@@ -1,5 +1,4 @@
 function loadScript(url, callback) {
-    // Adding the script tag to the head as suggested before
     var head = document.getElementsByTagName('head')[0];
     var script = document.createElement('script');
     script.type = 'text/javascript';
@@ -8,35 +7,31 @@ function loadScript(url, callback) {
     // There are several events for cross browser compatibility.
     script.onreadystatechange = callback;
     script.onload = callback;
-    // Fire the loading
     head.appendChild(script);
 }
-function decrypt() { // call encryption JS here
+function decrypt() {                                                                                    // call encryption JS here
     var encrypted_url = window.location.href;
     encrypted_url = url.substring(url.indexOf('#') + 1);
-    img_decrypt(url, fileChooser.files[0]); // pass to other JS file
+    var img = document.getElementById("hash");
+    // img.src = get_hash();
+    var div = document.getElementById("decoded");
+    div.innerHTML = decryptor(url, fileChooser.files[0]);                                           // pass to other JS file
 }
 var script_load = function() {
     decrypt("Hello!");
 };
-/* Creates an `input[type="file]` */
 var fileChooser = document.createElement('input');
 // var file;
 fileChooser.type = 'file';
 fileChooser.addEventListener('change', function () {
     // var file = fileChooser.files[0];
-    loadScript('resources/encrypt.js', script_load); // laod auxilary JS file
+    loadScript('resources/encrypt.js', script_load);                                                  // load auxilary JS file
     // var reader = new FileReader();
     // reader.onloadend = function () {
     //     image_str = reader.result;
     //     loadScript('resources/encrypt.js', script_load);
     // }
     // reader.readAsDataURL(file);
-
-    // var img = document.getElementById("hash");
-    // img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzK4Lu0EoLHfQFlkDWCLhppwbrx6dCXGsh0_6Lv4mdR-vM1IdavA";
-    // var div = document.getElementById("decoded");
-    // div.innerHTML = "test";
 });
 /* Wrap it in a form for resetting */
 // var form = document.createElement('form');       Maybe these two lines are not needed
@@ -57,18 +52,6 @@ function renderImage(url) {
     img = document.getElementById('hash');
     img.src = url;
 }
-// function getClickHandler() {
-//     return function(info, tab) {
-//         var url = 'display.html#' + info.srcUrl;
-//         chrome.tabs.create({url: url});
-//     };
-// };
-// chrome.contextMenus.create({
-//     "title" : "Decrypt Image",
-//     "type" : "normal",
-//     "contexts" : ["image"],
-//     "onclick" : getClickHandler()
-// });
 var clickHandler = function(info, tab) {
         var url = 'display.html#' + info.srcUrl;
         chrome.tabs.create({url: url});
@@ -77,6 +60,4 @@ chrome.contextMenus.create({
     "title": "Decrypt image",
     "contexts": ["image"]
 });
-// Must be synchronously called on event page load,
-//   for instance in the top level code
 chrome.contextMenus.onClicked.addListener(clickHandler);
